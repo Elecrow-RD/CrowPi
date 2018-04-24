9465853fc19365b0f6fb56784a2366bc0fadf526
Elecrow <Elecrow-keen@users.noreply.github.com>
2018-04-24 14:07:34 +0800

    touch buzzer initial upload

---
+++ touch_buzzer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# http://elecrow.com/
+
+import RPi.GPIO as GPIO
+import time
+
+# configure both button and buzzer pins
+button_pin = 11
+buzzer_pin = 12
+
+# set board mode to GPIO.BOARD
+GPIO.setmode(GPIO.BOARD)
+
+# setup button pin as input and buzzer pin as output
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(buzzer_pin, GPIO.OUT)
+
+try:
+    while True:
+        # check if button pressed
+        if(GPIO.input(button_pin)):
+            # set buzzer on
+            GPIO.output(buzzer_pin, GPIO.HIGH)
+        else:
+            # it's not pressed, set button off
+            GPIO.output(buzzer_pin, GPIO.LOW)
+except KeyboardInterrupt:
+    GPIO.cleanup()
