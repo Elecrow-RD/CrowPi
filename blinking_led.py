9bae579f210fe71f24014314cb71a820cbd6ad5a
Elecrow <Elecrow-keen@users.noreply.github.com>
2018-05-14 11:57:43 +0800

    Create blinking_led.py

---
+++ blinking_led.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# http://elecrow.com/
+
+import time
+import RPi.GPIO as GPIO
+
+# define LED pin
+led_pin = 37
+
+# set GPIO mode to GPIO.BOARD
+GPIO.setmode(GPIO.BOARD)
+# set puin as input
+GPIO.setup(led_pin, GPIO.OUT)
+
+try:
+    while True:
+        # turn on LED
+        GPIO.output(led_pin, GPIO.HIGH)
+        # Wait half a second
+        time.sleep(0.2)
+        # turn off LED
+        GPIO.output(led_pin, GPIO.LOW)
+        # Wait half a second
+        time.sleep(0.2)
+except KeyboardInterrupt:
+    # CTRL+C detected, cleaning and quitting the script
+    GPIO.cleanup()
