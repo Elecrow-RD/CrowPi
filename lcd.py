d85d594748e04b6c69961f685ee29aab57528723
Elecrow <Elecrow-keen@users.noreply.github.com>
2018-05-15 17:59:37 +0800

    Update lcd.py

--- lcd.py
+++ lcd.py
@@ -3,7 +3,6 @@
 
 # Example using a character LCD backpack.
 import time
-
 import Adafruit_CharLCD as LCD
 
 # Define LCD column and row size for 16x2 LCD.
@@ -13,53 +12,62 @@ lcd_rows    = 2
 # Initialize the LCD using the pins
 lcd = LCD.Adafruit_CharLCDBackpack(address=0x21)
 
-# Turn backlight on
-lcd.set_backlight(0)
+try:
+    # Turn backlight on
+    lcd.set_backlight(0)
 
-# Print a two line message
-lcd.message('Hello\nworld!')
+    # Print a two line message
+    lcd.message('Hello\nworld!')
 
-# Wait 5 seconds
-time.sleep(5.0)
+    # Wait 5 seconds
+    time.sleep(5.0)
 
-# Demo showing the cursor.
-lcd.clear()
-lcd.show_cursor(True)
-lcd.message('Show cursor')
+    # Demo showing the cursor.
+    lcd.clear()
+    lcd.show_cursor(True)
+    lcd.message('Show cursor')
 
-time.sleep(5.0)
+    time.sleep(5.0)
 
-# Demo showing the blinking cursor.
-lcd.clear()
-lcd.blink(True)
-lcd.message('Blink cursor')
+    # Demo showing the blinking cursor.
+    lcd.clear()
+    lcd.blink(True)
+    lcd.message('Blink cursor')
 
-time.sleep(5.0)
+    time.sleep(5.0)
 
-# Stop blinking and showing cursor.
-lcd.show_cursor(False)
-lcd.blink(False)
+    # Stop blinking and showing cursor.
+    lcd.show_cursor(False)
+    lcd.blink(False)
 
-# Demo scrolling message right/left.
-lcd.clear()
-message = 'Scroll'
-lcd.message(message)
-for i in range(lcd_columns-len(message)):
-    time.sleep(0.5)
-    lcd.move_right()
-for i in range(lcd_columns-len(message)):
-    time.sleep(0.5)
-    lcd.move_left()
+    # Demo scrolling message right/left.
+    lcd.clear()
+    message = 'Scroll'
+    lcd.message(message)
+    for i in range(lcd_columns-len(message)):
+        time.sleep(0.5)
+        lcd.move_right()
+    for i in range(lcd_columns-len(message)):
+        time.sleep(0.5)
+        lcd.move_left()
 
-# Demo turning backlight off and on.
-lcd.clear()
-lcd.message('Flash backlight\nin 5 seconds...')
-time.sleep(5.0)
-# Turn backlight off.
-lcd.set_backlight(1)
-time.sleep(2.0)
-# Change message.
-lcd.clear()
-lcd.message('Goodbye!')
-# Turn backlight on.
-lcd.set_backlight(0)
+    # Demo turning backlight off and on.
+    lcd.clear()
+    lcd.message('Flash backlight\nin 5 seconds...')
+    time.sleep(5.0)
+    # Turn backlight off.
+    lcd.set_backlight(1)
+    time.sleep(2.0)
+    # Change message.
+    lcd.clear()
+    lcd.message('Goodbye!')
+    # Turn backlight on.
+    lcd.set_backlight(0)
+    # Turn backlight off.
+    time.sleep(2.0)
+    lcd.clear()
+    lcd.set_backlight(1)
+except KeyboardInterrupt:
+    # Turn the screen off
+    lcd.clear()
+    lcd.set_backlight(1)
